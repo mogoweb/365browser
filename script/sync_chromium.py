a88d80e1f51dc4600acc600b0ca99b5ff60497da
alexzchen <alexzchen@china-liantong.com>
2015-10-30 23:02:43 +0800

    delete some duplicated files that already build in jars

--- script/sync_chromium.py
+++ script/sync_chromium.py
@@ -31,14 +31,16 @@ def sync_java_files(options):
                                  "gen", "enums")
     for dir in os.listdir(gen_enums_dir):
         java_dir = os.path.join(gen_enums_dir, dir)
-        sync(java_dir, app_java_dir, "sync")
+        args = {'exclude':['org/chromium/(android_webview|base|blink_public|content|content_public|media|net|sync|ui)\S*']}
+        sync(java_dir, app_java_dir, "sync", **args)
 
     # sync generated template files
     gen_template_dir = os.path.join(options.chromium_root, "out", options.buildtype,
                                  "gen", "templates")
     for dir in os.listdir(gen_template_dir):
         java_dir = os.path.join(gen_template_dir, dir)
-        sync(java_dir, app_java_dir, "sync")
+        args = {'exclude':['org/chromium/(android_webview|base|blink_public|content|content_public|media|net|sync|ui)\S*']}
+        sync(java_dir, app_java_dir, "sync", **args)
 
 def sync_jar_files(options):
     app_lib_dir = os.path.join(constants.DIR_APP_ROOT, "libs", "armeabi-v7a")
@@ -51,7 +53,7 @@ def sync_so_files(options):
     app_lib_dir = os.path.join(constants.DIR_APP_ROOT, "libs")
     chrome_java_lib_dir = os.path.join(options.chromium_root, "out", options.buildtype, "lib.java")
     args = {'only':['\w+_java\\.jar$', 'cacheinvalidation_javalib\\.jar$', 'jsr_305_javalib\\.jar$',
-                'protobuf_nano_javalib\\.jar$']}
+                'protobuf_nano_javalib\\.jar$', 'web_contents_delegate_android_java\\.jar$']}
     sync(chrome_java_lib_dir, app_lib_dir, "sync", **args)
 
 def sync_chromium_res_files(options):
