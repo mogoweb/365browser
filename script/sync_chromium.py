6ec9d2ba529e33efa518574d7037754f04b2bb1f
alexzchen <alexzchen@china-liantong.com>
2015-11-02 09:21:32 +0800

    don't sync chrome_jar.java

--- script/sync_chromium.py
+++ script/sync_chromium.py
@@ -61,7 +61,8 @@ def sync_jar_files(options):
     app_lib_dir = os.path.join(constants.DIR_APP_ROOT, "libs")
     chrome_java_lib_dir = os.path.join(options.chromium_root, "out", options.buildtype, "lib.java")
     args = {'only':['\w+_java\\.jar$', 'cacheinvalidation_javalib\\.jar$', 'jsr_305_javalib\\.jar$',
-                'protobuf_nano_javalib\\.jar$', 'web_contents_delegate_android_java\\.jar$']}
+                'protobuf_nano_javalib\\.jar$', 'web_contents_delegate_android_java\\.jar$'],
+            'ignore': ['chrome_java\\.jar$']}
     sync(chrome_java_lib_dir, app_lib_dir, "sync", **args)
 
 def sync_chromium_res_files(options):
